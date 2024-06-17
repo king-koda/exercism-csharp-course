3b44f66b4a4d60fbe198bffd7d5e354ad4d0036f
Deez <krisimus@gmail.com>
2024-06-17 14:41:43 +0800

    etc

--- ExercismCSharpTasks/obj/Debug/net8.0/ExercismCSharpTasks.AssemblyInfo.cs
+++ ExercismCSharpTasks/obj/Debug/net8.0/ExercismCSharpTasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercismCSharpTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5906ef09c1fd001f7ee37599d034d777c8d93821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1bfadd297081fe40f4798c76117f2ca08d98fdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercismCSharpTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercismCSharpTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
